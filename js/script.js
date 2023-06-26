7dce5f445e00afc77b403b8de258e58cf0eef6f7
Vida <134993237+VidaWee@users.noreply.github.com>
2023-06-26 19:56:20 +1200

    Add event listner to subscription selection and Duration

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 // Your JS code here
+
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#subscription");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener ("change", function(e){
+
+    subType = e.target.value;
+    console.log(subType);
+
+});
+
+subDurationElement.addEventListener("change", function(e){
+
+    subDuration = Number(e.target.value);
+    console.log(subDuration);
+});
